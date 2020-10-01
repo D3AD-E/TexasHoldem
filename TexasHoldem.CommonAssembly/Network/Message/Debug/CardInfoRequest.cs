7adfd0cd528ff32c7b21d531f6c0d374dc3e04ec
Yevhenii <evg.gordi@gmail.com>
2020-10-01 15:50:10 +0200

    Fix messages not getting to GameForm

---
+++ TexasHoldem.CommonAssembly/Network/Message/Debug/CardInfoRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TexasHoldemCommonAssembly.Game.Entities;
+
+namespace TexasHoldemCommonAssembly.Network.Message
+{
+    [Serializable]
+    public class CardInfoRequest : RequestMessageBase
+    {
+        public bool ToHand { get; set; }
+    }
+}
---
+++ TexasHoldem.CommonAssembly/Network/Message/Debug/JoinRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TexasHoldemCommonAssembly.Network.Message
+{
+    [Serializable]
+    public class JoinRequest : RequestMessageBase
+    {
+        public string Username { get; set; }
+    }
+}
